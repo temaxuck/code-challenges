5d8e37da3bd1d589d14ba74d80f52dccae103feb
temaxuck <gorropand@gmail.com>
2024-01-26 08:05:26 +0300

    Implement Decode WaysI (NeetCode's video implementation)

---
+++ leetcode/dynamic_programming/decode_ways/main.py
@@ -0,0 +1,28 @@
+class Solution:
+    VALID_RANGE = range(1, 27)
+    MAX_VALID_TOKEN_LENGTH = len(str(VALID_RANGE.stop - 1))
+
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        cache = {n: 1}
+
+        def helper(i):
+            print(i, cache)
+            if i in cache:
+                return cache[i]
+
+            if s[i] == "0":
+                return
+
+            res = helper(i + 1)
+
+            if i + 1 < n and int(s[i : i + 2]) in self.VALID_RANGE:
+                res += helper(i + 2)
+
+            cache[i] = res
+            return res
+
+        return helper(0)
+
+
+print(Solution().numDecodings("06"))
