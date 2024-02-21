205d148f88024ccfc309240f18f3182ebb2e6d05
temaxuck <gorropand@gmail.com>
2024-02-21 17:36:32 +0300

    Optimize palindromic_feature

--- yandex/palindromic_feature/main.py
+++ yandex/palindromic_feature/main.py
@@ -2,20 +2,22 @@ if __name__ == "__main__":
     s = input()
     n = len(s)
 
-    palindromes = []
+    min_palindrome = None
 
     def helper(lp, rp):
+        global min_palindrome
+
         while 0 <= lp and rp < n and s[lp] == s[rp]:
-            palindrome = s[lp : rp + 1]
-            if len(palindrome) > 1:
-                palindromes.append(palindrome)
+            cur_palindrome = s[lp : rp + 1]
+            if len(cur_palindrome) > 1 and cur_palindrome < (
+                min_palindrome or cur_palindrome + "a"
+            ):
+                min_palindrome = cur_palindrome
             lp -= 1
             rp += 1
 
     for i in range(n):
         helper(i, i)
         helper(i, i + 1)
-        if palindromes:
-            break
 
-    print(min(palindromes) if palindromes else -1)
+    print(min_palindrome or -1)
